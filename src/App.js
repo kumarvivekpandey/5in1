009eb5c2852573ab4ea11ee8a01dd7e584219c2b
Vivek Kumar <35356580+kumarvivekpandey@users.noreply.github.com>
2023-03-20 16:55:34 +0530

    changes done
    changes done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+
 function App() {
   return (
     <div className="App">
-        
-       <nav>
-     
-      <main>
-        <HashLink to={"/#home"}>Home</HashLink>
-        <Link to={"/contact"}>Contact</Link>
-        <HashLink to={"/#about"}>About</HashLink>
-        <HashLink to={"/#brands"}>Brands</HashLink>
-        <Link to={"/services"}>Services</Link>
-      </main>
-    </nav>
-      
-    
-      <h1>hhhhhhh</h1>  <h1>TechyStar.</h1>
+      <h1>ggggg</h1>
     </div>
   );
 }
 
 export default App;
-
